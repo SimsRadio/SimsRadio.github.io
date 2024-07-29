51c0026c01845ebdf65781af502f20803b7bb9d1
h <175410061+dyzancorn@users.noreply.github.com>
2024-07-29 22:17:41 +1200

    Use colour to highlight game column cells

--- src/components/utils/GridUtils.ts
+++ src/components/utils/GridUtils.ts
@@ -1,14 +1,14 @@
 import { CellClassParams, CellStyle } from 'ag-grid-community';
 import { rgba, complement } from 'polished';
 import { cornFlowerBlue } from '../../constants';
-import { MapleClient } from '../../models/MapleClient';
+import { SimsGame } from '../../models/SimsGame';
 
 export const ClientVersionCellStyle = (params: CellClassParams): CellStyle => {
   if (!params.value) return {};
   const color = rgba(cornFlowerBlue, 0.5);
   return {
-    backgroundColor: params.value.startsWith(MapleClient.Korea)
-      ? color
+    backgroundColor: SimsGame[params.value as keyof typeof SimsGame]
+      ? rgba(SimsGame[params.value as keyof typeof SimsGame], 0.5)
       : complement(color),
   };
 };
