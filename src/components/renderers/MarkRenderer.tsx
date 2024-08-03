42c2972513f9d63f80333da7b932143a038ee028
h <175410061+dyzancorn@users.noreply.github.com>
2024-08-04 02:01:24 +1200

    Add a tooltip to the mark renderer, showing the game pack, and if not, it just shows the base game name

--- src/components/renderers/MarkRenderer.tsx
+++ src/components/renderers/MarkRenderer.tsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import { ICellRendererParams } from 'ag-grid-community';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 export const MarkRenderer: React.FC<ICellRendererParams> = (params) => {
   const [imgSrc, setImgSrc] = useState(`mark/${params.value}`);
 
   return (
-    <span>
-      <img
-        src={imgSrc}
-        alt="icon"
-        width="35"
-        height="35"
-        onError={() => setImgSrc(`mark/default.png`)}
-      ></img>
-    </span>
+    <>
+      <OverlayTrigger
+        delay={{ show: 250, hide: 100 }}
+        overlay={
+          <Tooltip id={`tooltip-pack-name`}>
+            {params.data.pack
+              ? params.data.pack
+              : params.data.source.clientVersion}
+          </Tooltip>
+        }
+      >
+        <span>
+          <img
+            src={imgSrc}
+            alt="icon"
+            width="35"
+            height="35"
+            onError={() => setImgSrc(`mark/default.png`)}
+          ></img>
+        </span>
+      </OverlayTrigger>
+    </>
   );
 };
